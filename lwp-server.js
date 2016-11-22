03f5c50df31a5d4f7e7c6baa2eacd4a73b4c2434
mcowpl <mconline@macbookair-88ff.sg.tplgrp.com>
2016-11-22 12:00:47 +0800

    http console log again

--- lwp-server.js
+++ lwp-server.js
@@ -5,6 +5,7 @@ var server = http.createServer(function(request, response) {
     // process HTTP request. Since we're writing just WebSockets server
     // we don't have to implement anything.
     console.log("Incoming HTTP request from: " + request.ip);
+    console.log(request);
     response.end("For testing websockets. Not serving HTTP requests.");
 });
 var port = (process.env.PORT || 1337);
